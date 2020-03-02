1cb3bba15a30852db59c1f7af029a514a8ab6e84
QinmengLUAN <qinmengluan@gmail.com>
2020-03-02 23:43:58 +0000

    OOD question

---
+++ wk2_OOD_Matrix.py
@@ -0,0 +1,11 @@
+# Object Oriented Design (OOD)
+# First time to write OOD
+# OOD is like a package (i.e. object) with different functions
+
+# Task: write a matrix object with several functions
+# Initial function: m = Matrix(n) -> write a n*n matrix with all zeros 
+# m.set(1,2,3) -> set position (1,2) as 3
+# m.get(1,2) -> get the value of position (1.2)
+# m.print() -> customise how to print
+# m.rotate() -> clockwise rotate the matrix
+
