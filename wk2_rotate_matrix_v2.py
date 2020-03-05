2e001a53f88f5c4b1bcceafb502ebcb5f6011583
QinmengLUAN <qinmengluan@gmail.com>
2020-03-05 00:52:14 +0000

    Improve rotate matrix.py

--- wk2_rotate_matrix_v2.py
+++ wk2_rotate_matrix_v2.py
@@ -3,15 +3,25 @@
 # Strength: do not need to add space
 
 def rotateMatrix(matrix):
-	n = len(matrix)
-	for i in range((n + 1)//2):
-		for j in range(n - 1):
-			tmp = matrix[n - 1 - i][j]
+	m_len = len(matrix)
+	for depth in range(m_len//2): #tuple check  tuple vs list
+		init_pos = (depth,depth)
+		inner_len = m_len - depth * 2
+
+		for offset in range(inner_len - 1):
+
+			leftup_pos = (init_pos[0], init_pos[1] + offset)
+			rightup_pos = (init_pos[0] + offset, init_pos[1] + inner_len - 1)
+			rightdown_pos = (init_pos[0] + inner_len - 1, init_pos[1] + inner_len - 1 - offset)
+			leftdown_pos = (init_pos[0] + inner_len - 1 - offset, init_pos[1])
+
+			#SWAP
+			tmp = matrix[leftdown_pos[0]][leftdown_pos[1]]
 			print(tmp)
-			matrix[n - 1 - i][j] = matrix[n - 1- i][n - 1 - j]
-			matrix[n - 1- i][n - 1 - j] = matrix[i][n - 1 - j]
-			matrix[i][n - 1 - j] = matrix[i][j]
-			matrix[i][j] = tmp
+			matrix[leftdown_pos[0]][leftdown_pos[1]]  = matrix[rightdown_pos[0]][rightdown_pos[1]] 
+			matrix[rightdown_pos[0]][rightdown_pos[1]] = matrix[rightup_pos[0]][rightup_pos[1]]
+			matrix[rightup_pos[0]][rightup_pos[1]] = matrix[leftup_pos[0]][leftup_pos[1]]
+			matrix[leftup_pos[0]][leftup_pos[1]] = tmp
 
 	return matrix
 
@@ -20,3 +30,20 @@ def rotateMatrix(matrix):
 a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 print(rotateMatrix(a))
+
+
+#Testing
+def generate_matrix(n):
+  return [list(range(1 + i * n, 1 + i * n + n)) for i in range(n)]
+
+def print_matrix(matrix):
+  for row in matrix:
+    print(row)
+
+# Test below
+# m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+m = generate_matrix(9)
+print_matrix(m)
+rotate(m)
+print("------------------------------")
+print_matrix(m)
