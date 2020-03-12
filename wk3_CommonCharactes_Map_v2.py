ef9986259ba7146ca548a89f4aace58b5b6b9219
QinmengLUAN <qinmengluan@gmail.com>
2020-03-12 23:00:27 +0000

    Leetcode 1002

---
+++ wk3_CommonCharactes_Map_v2.py
@@ -0,0 +1,68 @@
+# Leetcode 1002. Find Common Characters
+"""
+Given an array A of strings made only from lowercase letters, 
+return a list of all characters that show up in all strings 
+within the list (including duplicates).  
+For example, if a character occurs 3 times in all strings but not 4 times, 
+you need to include that character three times in the final answer.
+
+Input: ["cool","lock","cook"]
+Output: ["c","o"]
+"""
+# Use map
+# Strength: it works for 
+#Input: ["bella","label","roller"]
+#Output: ["e","l","l"]
+
+class Solution:
+    def commonChars(self, A):
+        length = len(A)
+        dic_str = [{} for n in range(0,length)] # Important!!!
+        new_index = 1
+        for i in range(0,length):
+        	str_list = list(A[i])
+        	for j in range(0,len(str_list)):
+        		if str_list[j] not in dic_str[i].keys():
+        			dic_str[i].update({str_list[j]: 1})
+        		elif str_list[j] in dic_str[i].keys():
+        			new_index = dic_str[i].get(str_list[j]) + 1
+        			dic_str[i].update({str_list[j]: new_index})
+
+        dic_list = dic_str[0]
+
+        for i in range(1,length):
+        	for k in list(dic_list.keys()):
+        		if k not in list(dic_str[i].keys()):
+        			dic_list.pop(k)
+        		else:
+        			if dic_str[i].get(k) < dic_list.get(k):
+
+        				dic_list.update({k:dic_str[i].get(k)})
+        result = []
+        for k in dic_list.keys():
+        	num = dic_list.get(k)
+        	for i in range(num):
+        		result.append(k)
+
+        return result
+
+'''
+        for i in range(1,length):
+        	str_list = list(A[i])
+        	for k in range(0,len(str_list)):
+        		if str_list[j] not in dic_str.keys():
+        			dic_str.pop(str_list[j])
+        		else:
+        			dic_str.pop(str_list[j])
+
+
+        			new_index = dic_str[0].get(str_list[j]) + 1
+        		new_key = str_list[j]
+        		dic_str[0].update({new_key: new_index})
+
+        		  		
+        return dic_str
+'''
+s = Solution()
+inp = ["cooll","loockl","cookl"]
+print(s.commonChars(inp))
