91cc9306d62c5bb2dea813e75ff426e7b6558200
QinmengLUAN <qinmengluan@gmail.com>
2020-04-11 00:05:09 +0100

    Leetcode1290 Linked list

---
+++ wk7_getDecimalValue.py
@@ -0,0 +1,41 @@
+"""
+1290. Convert Binary Number in a Linked List to Integer
+Easy: Linked list
+Algorithm complexity:
+Time complexity: O(n).
+Space complexity: O(1).
+
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+Return the decimal value of the number in the linked list.
+
+Example 1:
+Input: head = [1,0,1]
+Output: 5
+Explanation: (101) in base 2 = (5) in base 10
+
+Example 2:
+Input: head = [0]
+Output: 0
+
+Example 3:
+Input: head = [1]
+Output: 1
+
+Example 4:
+Input: head = [1,0,0,1,0,0,1,1,1,0,0,0,0,0,0]
+Output: 18880
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        res = 0
+        while head != None:
+            res = res * 2 + head.val
+            head = head.next
+        return res
