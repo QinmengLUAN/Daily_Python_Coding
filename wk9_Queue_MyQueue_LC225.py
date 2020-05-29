bff64cbdcca09b082db8312e540520af359345e1
QinmengLUAN <qinmengluan@gmail.com>
2020-05-29 23:21:50 +0100

    Leetcode225 Stack

---
+++ wk9_Queue_MyQueue_LC225.py
@@ -0,0 +1,69 @@
+"""
+225. Implement Stack using Queues
+Easy
+
+Implement the following operations of a stack using queues.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+empty() -- Return whether the stack is empty.
+Example:
+
+MyStack stack = new MyStack();
+
+stack.push(1);
+stack.push(2);  
+stack.top();   // returns 2
+stack.pop();   // returns 2
+stack.empty(); // returns false
+"""
+class MyStack:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x onto stack.
+        """
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element on top of the stack and returns that element.
+        """
+        if self.empty == True:
+            return None
+        else:
+            return self.queue.pop()
+    
+    def top(self) -> int:
+        """
+        Get the top element.
+        """
+        if self.empty == True:
+            return None
+        else:
+            return self.queue[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the stack is empty.
+        """
+        if len(self.queue) == 0:
+            return True
+        else:
+            return False
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
