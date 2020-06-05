b642ba715452694da4c93cf0e1b4b7954f00212b
QinmengLUAN <qinmengluan@gmail.com>
2020-06-05 16:50:58 +0100

    Leetcode322 DP

--- wk10_DP_coinChange_LC322.py
+++ wk10_DP_coinChange_LC322.py
@@ -1,6 +1,6 @@
 """
 322. Coin Change
-Medium: DP
+Medium: DP, time complexity O(m*n), m is amount, n is the choice of coins
 
 You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 
