cc1daf60b8a3e0a9444a497777550613a9954233
Qinmeng Luan <31704318+QinmengLUAN@users.noreply.github.com>
2020-07-01 00:13:36 +0100

    Leetcode64 DP Path

--- wk10_DP_minPathSum_LC64.py
+++ wk10_DP_minPathSum_LC64.py
@@ -1,6 +1,7 @@
 """
 64. Minimum Path Sum
 Medium: table filling, time complexity O(M*N), space complexity O(M*N)
+Follow-up: return the shortest path
 ----------------------------------------------------------
 Codition: check the path to right, bottom
 Initial state: res[0][0] = grid[0][0]
@@ -41,6 +42,31 @@ class Solution:
         # print(res)
         return res[-1][-1]
 
+      
+    # Return the min Sum and shortest path    
+    def minPathSum_withPath(self, grid: List[List[int]]) -> int:    
+        res = [[[0] * 2 for _ in range(len(grid[0]))] for __ in range(len(grid))] 
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if i == 0 and j == 0:
+                    res[0][0][0] = grid[0][0]
+                    
+                elif i == 0 and j != 0:
+                    res[i][j][0] = res[i][j-1][0] + grid[i][j]
+                    res[i][j][1] = 'left'
+
+                elif j == 0 and i != 0:
+                    res[i][j][0] = res[i-1][j][0] + grid[i][j]
+                    res[i][j][1] = 'up'
+                else:
+                    res[i][j][0] = min(res[i-1][j][0], res[i][j-1][0]) + grid[i][j]
+                    if res[i-1][j][0] <= res[i][j-1][0]:
+                        res[i][j][1] = 'up'
+                    else:
+                        res[i][j][1] = 'left'
+        print(res)
+        return res[-1][-1][0]  
 s = Solution()
 grid = [[1,3,1],[1,5,1],[4,2,1]]
 print(s.minPathSum(grid))
