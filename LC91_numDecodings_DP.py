674e80a574eb8bb132163a4819f9671c43c5aeb7
QinmengLUAN <qinmengluan@gmail.com>
2020-07-14 16:44:06 +0100

    Leetcode91 DP

---
+++ LC91_numDecodings_DP.py
@@ -0,0 +1,48 @@
+"""
+91. Decode Ways
+Medium
+
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+Example 1:
+
+Input: "12"
+Output: 2
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+Example 2:
+
+Input: "226"
+Output: 3
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+"""
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s:
+            return 0
+
+        dp = [0 for x in range(len(s) + 1)] 
+
+        # base case initialization
+        dp[0] = 1 
+        dp[1] = 0 if s[0] == "0" else 1   #(1)
+
+        for i in range(2, len(s) + 1): 
+            # One step jump
+            if 0 < int(s[i-1:i]) <= 9:    #(2)
+                dp[i] += dp[i - 1]
+            # Two step jump
+            if 10 <= int(s[i-2:i]) <= 26: #(3)
+                dp[i] += dp[i - 2]
+        # print(dp)
+        return dp[len(s)]
+"""
+Notes of Wisdom:
+(1): Handling s starting with '0'. Alternative: I would recommend treating as an error condition and immediately returning 0. It's easier to keep track and it's an optimization.
+(2) (3): Pay close attention to your comparators. For (1) you want 0 <, not 0 <= . For (2) you want 10 <=, not 10 <
+"""
