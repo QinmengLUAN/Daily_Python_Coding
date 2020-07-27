5a7b0a74b645460b47dfad1a932ad09e4c19ec8d
QinmengLUAN <qinmengluan@gmail.com>
2020-07-27 14:08:11 +0100

    GS_MaximalCommonality

--- GS_coding/GS_MaximalCommonality/GS_MaximalCommonality.py
+++ GS_coding/GS_MaximalCommonality/GS_MaximalCommonality.py
@@ -1,18 +1,41 @@
+# Solution 1: use list to store appeared times
+class Solution1:
+    def MaximalCommonality(self, stri):
+        count = [0 for _ in range(26)]
+        for i in stri:
+            count[ord(i) - 97] += 1
+
+        res = 0
+        cur = 0
+        for i in stri:
+            if count[ord(i) - 97] > 1:
+                cur += 1
+                count[ord(i) - 97] -= 2
+            elif count[ord(i) - 97] == 0:
+                cur -= 1
+            res = max(cur, res)
+        return res
+
+# Solution 2: use Counter
 from collections import Counter
 class Solution:
-    def MaximalCommonality(self, s):
-        c = Counter(s)
-        cnt = 0
-
-        for ch in s:
-            c[ch] -= 1
-            if c[ch] > 0:
-                cnt += 1
-        return cnt
+    def MaximalCommonality(self, stri):
+        count = Counter(stri)
+        res = 0
+        cur = 0
+        for i in stri:
+            if count[i] > 1:
+                cur += 1
+                count[i] -= 2
+            elif count[i] == 0:
+                cur -= 1
+            res = max(cur, res)
+        return res
 
 s = Solution()
 inp = "abcdedear"
 inp = "abcdecdefg"
 inp = "aaaa"
-inp = "abcabca"
+# inp = "abcabca"
+# inp = "aaa"
 print(s.MaximalCommonality(inp))
