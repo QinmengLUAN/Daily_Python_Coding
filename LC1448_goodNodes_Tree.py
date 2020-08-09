4f7b82327bcf8d458afd0e9816336c5af83741d0
QinmengLUAN <qinmengluan@gmail.com>
2020-08-09 22:08:08 +0100

    Leetcode1448 Tree Recursion

---
+++ LC1448_goodNodes_Tree.py
@@ -0,0 +1,51 @@
+"""
+1448. Count Good Nodes in Binary Tree
+Medium: Tree, Recursion
+
+Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+
+Return the number of good nodes in the binary tree.
+
+ 
+
+Example 1:
+Input: root = [3,1,4,3,null,1,5]
+Output: 4
+Explanation: Nodes in blue are good.
+Root Node (3) is always a good node.
+Node 4 -> (3,4) is the maximum value in the path starting from the root.
+Node 5 -> (3,4,5) is the maximum value in the path
+Node 3 -> (3,1,3) is the maximum value in the path.
+
+Example 2:
+Input: root = [3,3,null,4,2]
+Output: 3
+Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
+
+Example 3:
+Input: root = [1]
+Output: 1
+Explanation: Root is considered as good.
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        res = 0
+        res += self.helper(root, root.val)
+        return res
+    
+    def helper(self, node, val):
+        if node == None:
+            return 0
+        res = 0
+        if node.val >= val:
+            val = node.val
+            res += 1
+        res += self.helper(node.left, val)
+        res += self.helper(node.right, val)
+        return res
