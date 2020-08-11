b0ac6866809c2402a0d1fefe43c0eacdbf1dc479
QinmengLUAN <qinmengluan@gmail.com>
2020-08-11 23:24:08 +0100

    Leetcode387 String Hash Table

--- LC387_firstUniqChar_String.py
+++ LC387_firstUniqChar_String.py
@@ -13,10 +13,22 @@ s = "loveleetcode"
 return 2.
 """
 class Solution:
-    def firstUniqChar(self, s: str) -> int:
+    def firstUniqChar1(self, s: str) -> int:
         c = Counter(s)
         res = len(s)
         for i in range(len(s)):
             if c[s[i]] == 1:
                 res = min(res, i)
         return res if res != len(s) else -1
+
+
+    def firstUniqChar(self, s: str) -> int:
+        seen = set()
+        d = dict()
+        for i in range(len(s)):
+            if s[i] not in seen:
+                seen.add(s[i])
+                d[s[i]] = i
+            elif s[i] in d:
+                d.pop(s[i])
+        return min(d.values()) if d else -1
