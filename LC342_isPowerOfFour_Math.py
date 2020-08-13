2b3fb362f3416dc00309f3900694a13d9bad0fb2
QinmengLUAN <qinmengluan@gmail.com>
2020-08-13 13:50:00 +0100

    Leetcode342 Math

--- LC342_isPowerOfFour_Math.py
+++ LC342_isPowerOfFour_Math.py
@@ -13,9 +13,12 @@ Output: false
 Follow up: Could you solve it without loops/recursion?
 """
 class Solution:
-    def isPowerOfFour(self, num: int) -> bool:
+    def isPowerOfFour1(self, num: int) -> bool:
         if (num <= 0):
             return False
         while num % 4 == 0:
                num //= 4
         return num == 1
+
+    def isPowerOfFour(self, num):
+        return num > 0 and num & (num-1) == 0 and 0b1010101010101010101010101010101 & num == num
