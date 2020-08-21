3ec815e0eb4d8ebba5e7501b93250ed374b4857a
QinmengLUAN <qinmengluan@gmail.com>
2020-08-22 00:42:34 +0100

    Leetcode954 Lambda

---
+++ LC954_isAlienSorted_HashTable.py
@@ -0,0 +1,34 @@
+"""
+953. Verifying an Alien Dictionary
+Easy: Hash Table, lambda
+
+In an alien language, surprisingly they also use english lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographicaly in this alien language.
+
+ 
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+"""
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_dict = {}
+        for i in range(len(order)):
+            order_dict[order[i]] = i
+        ## Or:          
+        # order_dict ={c:i for i,c in enumerate(order)}
+        return words == sorted(words, key = lambda w: [order_dict[x] for x in w])
