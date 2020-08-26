4a2f68f658ad562926632bffc594e8d346d5db87
QinmengLUAN <qinmengluan@gmail.com>
2020-08-26 14:51:10 +0100

    Leetcode1513 Math

---
+++ LC1513_numSub_Math_String.py
@@ -0,0 +1,56 @@
+"""
+1513. Number of Substrings With Only 1s
+Medium: Math, String
+
+Given a binary string s (a string consisting only of '0' and '1's).
+
+Return the number of substrings with all characters 1's.
+
+Since the answer may be too large, return it modulo 10^9 + 7.
+
+ 
+
+Example 1:
+
+Input: s = "0110111"
+Output: 9
+Explanation: There are 9 substring in total with only 1's characters.
+"1" -> 5 times.
+"11" -> 3 times.
+"111" -> 1 time.
+Example 2:
+
+Input: s = "101"
+Output: 2
+Explanation: Substring "1" is shown 2 times in s.
+Example 3:
+
+Input: s = "111111"
+Output: 21
+Explanation: Each substring contains only 1's characters.
+Example 4:
+
+Input: s = "000"
+Output: 0
+ 
+
+Constraints:
+
+s[i] == '0' or s[i] == '1'
+1 <= s.length <= 10^5
+Accepted
+14,722
+Submissions
+36,397
+Seen this question in a real interview before?
+
+Yes
+
+No
+Contributor
+LeetCode
+Count number of 1s in each consecutive-1 group. For a group with n consecutive 1s, the total contribution of it to the final answer is (n + 1) * n // 2.
+"""
+class Solution:
+    def numSub(self, s: str) -> int:
+        return sum([(len(n) + 1) * len(n) // 2 for n in s.split("0")]) % (10**9 + 7)
