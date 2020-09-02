40c7017e94f344004f08fff942c78621f1874c09
QinmengLUAN <qinmengluan@gmail.com>
2020-09-02 20:31:01 +0100

    Leetcode647 DP

--- LC647_countSubstrings_StringDP.py
+++ LC647_countSubstrings_StringDP.py
@@ -20,6 +20,19 @@ Output: 6
 Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
 """
 class Solution:
+    # Improved version; table filling from from the bottom
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        
+        res = 0
+        for i in range(n-1, -1, -1):
+            for j in range(i, n):
+                dp[i][j] = s[i] == s[j] and ((j-i+1) < 3 or dp[i+1][j-1])
+                res += dp[i][j]
+        return res
+
+class Solution:
     def countSubstrings(self, s: str) -> int:
         len_s = len(s)
         resT = [[False] * len_s for i in range(len_s)]
