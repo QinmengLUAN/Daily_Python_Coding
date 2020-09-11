1bf1731ce08a4a9e7f9bcd046d6c658e001955bb
QinmengLUAN <qinmengluan@gmail.com>
2020-09-11 22:41:04 +0100

    DailyProblem10 Leetcode66

--- DailyProblem10_check_Array.py
+++ DailyProblem10_check_Array.py
@@ -1,6 +1,6 @@
 """
 Hi, here's your problem today. This problem was recently asked by Microsoft:
-
+Leetcode 665
 You are given an array of integers in an arbitrary order. Return whether or not it is possible to make the array non-decreasing by modifying at most 1 element to any value.
 
 We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
@@ -22,23 +22,22 @@ print check([5,1,3,2,5])
 # False
 
 Can you find a solution in O(n) time?
+从头开始模拟，碰到第一次nums[i]<nums[i-1]时，
+我们需要修改nums[i]或者nums[i-1]来保证数组的不下降。 
+有两种情况： 1、nums[i]<nums[i-2]，比如3,4,2这样的情况，当前nums[i]=2。此时我们只能将nums[i]修改为4，才能在满足题意的条件下保证数组不下降，修改后为3,4,4。 
+2、nums[i]>=nums[i-2]，比如3,5,4，当前nums[i]=4。此时我们可以将nums[i-1]修改为4，修改后为3,4,4。
 """
-def check(lst):
+def check(nums):
   # Fill this in.
-    cnt_equal, cnt_drop = 0, 0
-    for i in range(len(lst)-1):
-        if lst[i] == lst[i+1]:
-            cnt_equal += 1
-        elif lst[i] < lst[i+1]:
-            if i < len(lst) - 2:
-                cnt_equal = 0
-        else:
-            if i < len(lst) - 2:
-                cnt_drop += cnt_equal
-            cnt_equal = 0
-            cnt_drop += 1
-        # print(cnt_drop, cnt_equal)
-    return (cnt_drop + cnt_equal) <= 1
+    count = 0
+    for i in range(1, len(nums)):
+        if nums[i] < nums[i - 1]:
+            count += 1
+            if i >= 2 and nums[i] < nums[i - 2]:
+                nums[i] = nums[i - 1]
+            else:
+                nums[i - 1] = nums[i]
+    return count <= 1
  
 print(check([13, 4, 7]))
 # True
@@ -46,7 +45,7 @@ print(check([5,1,3,2,5]))
 # False
 print(check([1,1,0,0,3,3]))
 # False
-print(check([1,1,0]))
+print(check([1,9,1, 1]))
 # True
 print(check([1]))
 # True
