fa0be83a5fe10c84c20364fca97f535c9c172ea6
QinmengLUAN <qinmengluan@gmail.com>
2020-09-15 21:05:18 +0100

    DailyProblem05 Array

---
+++ DailyProblem05_getRange_Array.py
@@ -0,0 +1,46 @@
+"""
+Hi, here's your problem today. This problem was recently asked by AirBNB:
+
+Given a sorted array, A, with possibly duplicated elements, find the indices of the first and last occurrences of a target element, x. Return -1 if the target is not found.
+
+Example:
+Input: A = [1,3,3,5,7,8,9,9,9,15], target = 9
+Output: [6,8]
+
+Input: A = [100, 150, 150, 153], target = 150
+Output: [1,2]
+
+Input: A = [1,2,3,4,5,6,10], target = 9
+Output: [-1, -1]
+Here is a function signature example:
+
+class Solution: 
+  def getRange(self, arr, target):
+    # Fill this in.
+  
+# Test program 
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
+x = 2
+print(Solution().getRange(arr, x))
+# [1, 4]
+"""
+class Solution: 
+  def getRange(self, arr, target):
+    # Fill this in.
+    res = []
+    for i in range(len(arr)):
+        if arr[i] == target:
+            if len(res) == 2:
+                res.pop()
+            res.append(i)
+    if len(res) == 2:
+        return res
+    else:
+        return -1
+
+  
+# Test program 
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
+x = 2
+print(Solution().getRange(arr, x))
+# [1, 4]
