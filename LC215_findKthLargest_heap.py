95e73bb4e98ec63273dc63b565c4d44d20729ed5
Qinmeng Luan <31704318+QinmengLUAN@users.noreply.github.com>
2020-10-19 22:59:16 +0100

    Update LC215_findKthLargest_heap.py

--- LC215_findKthLargest_heap.py
+++ LC215_findKthLargest_heap.py
@@ -16,6 +16,42 @@ Input: [3,2,3,1,2,4,5,5,6] and k = 4
 Output: 4
 """
 class Solution:
+    # Quick sort
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        left = []
+        right = []
+        equal = []
+        pivot = random.choice(nums)
+        for num in nums:
+            if num < pivot:
+                left.append(num)
+            elif num == pivot:
+                equal.append(num)
+            else:
+                right.append(num)
+                
+        if len(right) == k - 1:
+            return pivot
+        
+        if len(right) > k - 1:
+            return self.findKthLargest(right, k)
+            
+        if len(equal) >= k - len(right):
+            return pivot
+        
+        return self.findKthLargest(left, k - len(right) - len(equal))
+            
+    # heap
+    def findKthLargest2(self, nums: List[int], k: int) -> int:
+        hq = []
+        for num in nums:
+            heapq.heappush(hq, num)
+            if len(hq) > k:
+                heapq.heappop(hq)              
+        return heapq.heappop(hq)
+
+class Solution:
+    # reverse heap
     def findKthLargest(self, nums: List[int], k: int) -> int:
         hp = []
         for n in nums:
