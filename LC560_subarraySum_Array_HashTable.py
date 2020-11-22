43506b898fc803aa8c4828d7f08a88e145e2430c
Qinmeng Luan <31704318+QinmengLUAN@users.noreply.github.com>
2020-11-22 00:22:42 +0000

    Update LC560_subarraySum_Array_HashTable.py

--- LC560_subarraySum_Array_HashTable.py
+++ LC560_subarraySum_Array_HashTable.py
@@ -9,6 +9,23 @@ Example 1:
 Input:nums = [1,1,1], k = 2
 Output: 2
 """
+# Method 1: dictionary
+# 首先求出nums的前缀和数组 然后将前缀和数组扫一遍，每扫到一个位置就将答案加上前面(k-prefixSum)出现次数（出现次数可以用dict维护） 再将当前前缀和prefixSum在出现的次数+1
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        for i in range(1, len(nums)):
+        	nums[i] += nums[i - 1]
+        d, ans = {0 : 1}, 0
+        for i in range(len(nums)):
+        	if(d.get(nums[i] - k) != None):
+        		ans += d[nums[i] - k]
+        	if(d.get(nums[i]) == None):
+        		d[nums[i]] = 1
+        	else:
+        		d[nums[i]] += 1
+        return ans
+
+# Method 2
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         r_dic = {0:[len(nums)]}
