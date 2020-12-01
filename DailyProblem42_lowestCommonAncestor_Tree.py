5de10025f28a04d89acdf8e108d55f418b1384b5
QinmengLUAN <qinmengluan@gmail.com>
2020-12-01 00:19:16 +0000

    lowestCommonAncestor Tree recursion

--- DailyProblem42_lowestCommonAncestor_Tree.py
+++ DailyProblem42_lowestCommonAncestor_Tree.py
@@ -42,7 +42,37 @@ class TreeNode:
 
 
 def lowestCommonAncestor(root, a, b):
-    # Fill this in.
+    # Fill this in. Recursion to find a and b, return the root
+    # If both a 和 b exist，return  LCA
+    # If only a exists，return a
+    # If only b exists，return b
+    # If neither a or b exists，return  LCA
+
+    if root == None:
+        return None
+        
+    if root == a or root == b:
+        return root
+
+    left_result = lowestCommonAncestor(root.left, a, b)
+    right_result = lowestCommonAncestor(root.right, a, b)
+
+    # A 和 B 一边一个
+    if left_result and right_result: 
+        return root
+    
+    # 左子树有一个点或者左子树有LCA
+    if left_result:
+        return left_result
+    
+    # 右子树有一个点或者右子树有LCA
+    if right_result:
+        return right_result
+    
+    # 左右子树啥都没有
+    return None
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 #   a
 #  / \
