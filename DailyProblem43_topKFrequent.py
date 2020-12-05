005d14ee87ce442cddfa3e4c6e7541512e830771
QinmengLUAN <qinmengluan@gmail.com>
2020-12-05 23:59:28 +0000

    k

---
+++ DailyProblem43_topKFrequent.py
@@ -0,0 +1,33 @@
+"""
+Hi, here's your problem today. This problem was recently asked by AirBNB:
+
+Given a non-empty list of words, return the k most frequent words. The output should be sorted from highest to lowest frequency, and if two words have the same frequency, the word with lower alphabetical order comes first. Input will contain only lower-case letters.
+
+Example:
+Input: ["daily", "interview", "pro", "pro", 
+"for", "daily", "pro", "problems"], k = 2
+Output: ["pro", "daily"]
+class Solution(object):
+  def topKFrequent(self, words, k):
+    # Fill this in.
+
+words = ["daily", "interview", "pro", "pro", "for", "daily", "pro", "problems"]
+k = 2
+print(Solution().topKFrequent(words, k))
+# ['pro', 'daily']
+"""
+from collections import Counter
+class Solution(object):
+  def topKFrequent(self, words, k):
+        c = Counter(words)
+        sorted(c, key=c.get, reverse=True)
+        res = []
+        for i in range(min(k, len(c))):
+            res.append(c[i].items)
+        return res
+
+
+words = ["daily", "interview", "pro", "pro", "for", "daily", "pro", "problems"]
+k = 2
+print(Solution().topKFrequent(words, k))
+# ['pro', 'daily']
