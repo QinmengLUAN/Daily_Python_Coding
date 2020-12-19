a0093675055bbf5e896c8c691742e935d97e4776
QinmengLUAN <qinmengluan@gmail.com>
2020-12-19 23:47:09 +0000

    findTime

---
+++ DailyProblem57_findTime.py
@@ -0,0 +1,27 @@
+"""
+Hi, here's your problem today. This problem was recently asked by AirBNB:
+
+We have a list of tasks to perform, with a cooldown period. We can do multiple of these at the same time, but we cannot run the same task simultaneously.
+
+Given a list of tasks, find how long it will take to complete the tasks in the order they are input.
+tasks = [1, 1, 2, 1]
+cooldown = 2
+output: 7 (order is 1 _ _ 1 2 _ 1)
+def findTime(arr, cooldown):
+  # Fill this in.
+
+print findTime([1, 1, 2, 1], 2)
+# 7
+"""
+import collections
+def findTime(arr, cooldown):
+    c = collections.Counter(arr)
+    max_count = max(max(c.values()), len(c))
+    return (max_count-1) * cooldown + max_count
+
+print(findTime([1, 1, 2, 1], 2))
+# 7
+print(findTime([1, 1, 1], 2))
+# 7
+print(findTime([1, 2, 2, 1], 2))
+# 4
