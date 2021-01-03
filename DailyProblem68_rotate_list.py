58968b1d7869b7f64ff15bc8bdfe12b34ca40487
QinmengLUAN <qinmengluan@gmail.com>
2021-01-03 22:24:49 +0000

    rotate_list

--- DailyProblem68_rotate_list.py
+++ DailyProblem68_rotate_list.py
@@ -43,24 +43,26 @@ class Node:
 
 def rotate_list(llist, k):
   # Fill this in.
-  root = node = llist
-  cnt  = 1
-  while cnt < k:
+  length = 1
+  node = llist
+  while node.next:
+    length += 1
     node = node.next
+  node.next = llist
+
+
+  tmp = llist
+  cnt  = 1
+  while cnt < (length-k):
+    tmp = tmp.next
     cnt += 1
-
-  nroot = nnode = node.next
-  node.next = None
-
-  while nnode.next:
-    nnode = nnode.next
-
-  nnode.next = root
+  nroot = tmp.next
+  tmp.next = None
   return nroot
 
-# Order is 1, 2, 3, 4
-llist = Node(1, Node(2, Node(3, Node(4))))
+# Order is 1, 2, 3, 4, 5
+llist = Node(1, Node(2, Node(3, Node(4, Node(5)))))
 
-# Order should now be 3, 4, 1, 2
+# Order should now be 4, 5, 1, 2, 3
 print(rotate_list(llist, 2))
-# 3412
+# 45123
